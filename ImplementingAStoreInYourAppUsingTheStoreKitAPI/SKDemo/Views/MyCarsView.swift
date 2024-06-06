27876176b1804f13d847e4c7d65e081c512f1e40
monkey6468 <>
2024-06-06 14:30:55 +0800

    ProductDetailView移除

--- ImplementingAStoreInYourAppUsingTheStoreKitAPI/SKDemo/Views/MyCarsView.swift
+++ ImplementingAStoreInYourAppUsingTheStoreKitAPI/SKDemo/Views/MyCarsView.swift
@@ -13,88 +13,29 @@ struct MyCarsView: View {
 
     var body: some View {
         NavigationView {
-            if store.purchasedCars.isEmpty && store.purchasedNonRenewableSubscriptions.isEmpty && store.purchasedSubscriptions.isEmpty {
-                VStack {
-                    Text("SK Demo App")
-                        .bold()
-                        .font(.system(size: 50))
-                        .padding(.bottom, 20)
-                    Text("🏎💨")
-                        .font(.system(size: 120))
-                        .padding(.bottom, 20)
-                    Text("Head over to the shop to get started!")
+            VStack {
+                Text("SK Demo App")
+                    .bold()
+                    .font(.system(size: 50))
+                    .padding(.bottom, 20)
+                Text("🏎💨")
+                    .font(.system(size: 120))
+                    .padding(.bottom, 20)
+                Text("Head over to the shop to get started!")
+                    .font(.headline)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                NavigationLink {
+                    StoreView()
+                } label: {
+                    Label("Shop", systemImage: "cart")
                         .font(.headline)
+                        .foregroundColor(.white)
                         .padding()
-                        .multilineTextAlignment(.center)
-                    NavigationLink {
-                        StoreView()
-                    } label: {
-                        Label("Shop", systemImage: "cart")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(width: 300, height: 50, alignment: .center)
-                            .background(Color.blue)
-                            .cornerRadius(15.0)
-                    }
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .background(Color.blue)
+                        .cornerRadius(15.0)
                 }
-            } else {
-                List {
-                    Section("My Cars") {
-                        if !store.purchasedCars.isEmpty {
-                            ForEach(store.purchasedCars) { product in
-                                NavigationLink {
-                                    ProductDetailView(product: product)
-                                } label: {
-                                    ListCellView(product: product, purchasingEnabled: false)
-                                }
-                            }
-                        } else {
-                            Text("You don't own any car products. \nHead over to the shop to get started!")
-                        }
-                    }
-                    Section("Navigation Service") {
-                        if !store.purchasedNonRenewableSubscriptions.isEmpty || !store.purchasedSubscriptions.isEmpty {
-                            ForEach(store.purchasedNonRenewableSubscriptions) { product in
-                                NavigationLink {
-                                    ProductDetailView(product: product)
-                                } label: {
-                                    ListCellView(product: product, purchasingEnabled: false)
-                                }
-                            }
-                            ForEach(store.purchasedSubscriptions) { product in
-                                NavigationLink {
-                                    ProductDetailView(product: product)
-                                } label: {
-                                    ListCellView(product: product, purchasingEnabled: false)
-                                }
-                            }
-
-                        } else {
-                            if let subscriptionGroupStatus = store.subscriptionGroupStatus {
-                                if subscriptionGroupStatus == .expired || subscriptionGroupStatus == .revoked {
-                                    Text("Welcome Back! \nHead over to the shop to get started!")
-                                } else if subscriptionGroupStatus == .inBillingRetryPeriod {
-                                    //The best practice for subscriptions in the billing retry state is to provide a deep link
-                                    //from your app to https://apps.apple.com/account/billing.
-                                    Text("Please verify your billing details.")
-                                }
-                            } else {
-                                Text("You don't own any subscriptions. \nHead over to the shop to get started!")
-                            }
-                        }
-                    }
-                    
-                    NavigationLink {
-                        StoreView()
-                    } label: {
-                        Label("Shop", systemImage: "cart")
-                    }
-                    .foregroundColor(.white)
-                    .listRowBackground(Color.blue)
-                }
-                
-                .navigationTitle("SK Demo App")
             }
         }
         .environmentObject(store)
@@ -107,3 +48,10 @@ struct MyCarsView_Previews: PreviewProvider {
     }
 }
 
+extension Date {
+    func formattedDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
